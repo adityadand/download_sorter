ac91e902215bc9cd70ccac48049eebeaf6c346f9
aditya dand <33375699+adityadand@users.noreply.github.com>
2025-09-21 21:32:46 +0530

    Create powershell_file_info.ps1
    this shows  Name  Count TotalSize(MB) of all extension in powershell

---
+++ powershell_file_info.ps1
@@ -0,0 +1,8 @@
+cd $env:USERPROFILE\Downloads
+Get-ChildItem -File |
+    Group-Object Extension |
+    Select-Object Name,
+                  @{Name='Count'; Expression={$_.Count}},
+                  @{Name='TotalSize(MB)'; Expression={[math]::Round(($_.Group | Measure-Object Length -Sum).Sum / 1MB,2)}} |
+    Sort-Object Count -Descending |
+    Format-Table -AutoSize
