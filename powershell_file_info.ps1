0e5b17dfecae6e4a4abb00e28672fed54d1e8b3b
aditya dand <33375699+adityadand@users.noreply.github.com>
2025-09-21 21:34:09 +0530

    Update powershell_file_info.ps1
    added AverageSize(MB) also

--- powershell_file_info.ps1
+++ powershell_file_info.ps1
@@ -3,6 +3,7 @@ Get-ChildItem -File |
     Group-Object Extension |
     Select-Object Name,
                   @{Name='Count'; Expression={$_.Count}},
-                  @{Name='TotalSize(MB)'; Expression={[math]::Round(($_.Group | Measure-Object Length -Sum).Sum / 1MB,2)}} |
+                  @{Name='TotalSize(MB)'; Expression={[math]::Round(($_.Group | Measure-Object Length -Sum).Sum / 1MB,2)}},
+                  @{Name='AverageSize(MB)'; Expression={[math]::Round(($_.Group | Measure-Object Length -Average).Average / 1MB,2)}} |
     Sort-Object Count -Descending |
     Format-Table -AutoSize
